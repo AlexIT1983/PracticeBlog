b9f1190a329a1604b45dba4b52463dcd65f4a714
Alex Antipov <alex.antipov@gmail.com>
2024-03-28 01:07:52 +0300

    Настроить проект

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const Div = styled.div`
 	text-align: center;
 	text-decoration: bold;
-	font-size: 20px;
+	font-size: 22px;
 `;
 
 export const App = () => {
